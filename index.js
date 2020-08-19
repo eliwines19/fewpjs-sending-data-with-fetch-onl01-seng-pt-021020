2f5aec14740c3c1ed536f220a7320fb647872832
Elijah wines <elijahwines84@yahoo.com>
2020-08-19 12:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  let formData = { userName: name, userEmail: email }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("couldn't fetch the data");
+    console.log(error.message);
+  });
+};
